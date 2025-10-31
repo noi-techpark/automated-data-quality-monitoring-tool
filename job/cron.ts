ea1e8a0d3b8decf9fe51c03f38b49a592f45ca27
Davide Montesin <davide.montesin@catch-solve.tech>
2025-10-31 12:49:04 +0100

    draft for more clients

--- job/cron.ts
+++ job/cron.ts
@@ -1,5 +1,52 @@
 import cron from 'node-cron';
 
+console.log("\r\n\r\n          _ _               _           _           \r\n  ___  __| | |_    ___   __| |_  ___ __| |_____ _ _ \r\n \/ _ \\\/ _` | \' \\  |___| \/ _| \' \\\/ -_) _| \/ \/ -_) \'_|\r\n \\___\/\\__,_|_||_|       \\__|_||_\\___\\__|_\\_\\___|_|  \r\n                                                    \r\n\r\n");
+
+const envValues: Record<string, string | undefined> = {};
+const missingEnvKeys: string[] = [];
+
+
+function requireEnvVar(key: string, defaultValue?: string): string {
+  let value = process.env[key] ?? defaultValue;
+  if (value === undefined)
+  {
+    missingEnvKeys.push(key);
+    value = ''
+  }
+  envValues[key] = value;
+  return value;
+}
+
+export const KEYCLOAK_CLIENT_ID_OPENDATA = 'opendata';
+
+export const LOG_LEVEL = requireEnvVar('LOG_LEVEL', 'minimal');
+export const DATABASE_URL = requireEnvVar('DATABASE_URL');
+export const KEYCLOAK_BASE_URL = requireEnvVar('KEYCLOAK_BASE_URL');
+export const METADATA_BASE_URL = requireEnvVar('METADATA_BASE_URL');
+export const KEYCLOAK_ACCOUNTS_JSON = requireEnvVar('KEYCLOAK_ACCOUNTS_JSON');
+export const DATASET_CONTENT_PAGE_LIMIT = requireEnvVar('DATASET_CONTENT_PAGE_LIMIT');
+export const DEBUG_MODE_CACHE_ON = requireEnvVar('DEBUG_MODE_CACHE_ON', 'false').trim().toLowerCase() === 'true';
+
+
+Object.entries(envValues).forEach(([key, value]) => {
+  const ok = missingEnvKeys.includes(key) ? '❌' : '✅';
+  console.log(`${ok} ${key} = ${value}`);
+});
+
+if (missingEnvKeys.length > 0) {
+  console.error(`Give up, missing required environment variable, look for ❌`);
+  process.exit(1);
+}
+
+
+/*
+export const KEYCLOAK_ASSOCIATED_ROLE = requireEnvVar('KEYCLOAK_ASSOCIATED_ROLE', KEYCLOAK_CLIENT_ID_OPENDATA)
+export const KEYCLOAK_CLIENT_ID = requireEnvVar('KEYCLOAK_CLIENT_ID', KEYCLOAK_CLIENT_ID_OPENDATA);
+export const KEYCLOAK_REALM = requireEnvVar('KEYCLOAK_REALM','');
+export const KEYCLOAK_CLIENT_SECRET = requireEnvVar('KEYCLOAK_CLIENT_SECRET','');
+ */
+
+
 cron.schedule('* * * * *', () => {
   console.log('running a task every minute');
 });
--- job/index.ts
+++ job/index.ts
@@ -7,46 +7,6 @@ import { xml2json } from 'xml-js';
 
 import {fetch_json_with_optional_cache} from "./lib/utils";
 
-console.log("\r\n\r\n          _ _               _           _           \r\n  ___  __| | |_    ___   __| |_  ___ __| |_____ _ _ \r\n \/ _ \\\/ _` | \' \\  |___| \/ _| \' \\\/ -_) _| \/ \/ -_) \'_|\r\n \\___\/\\__,_|_||_|       \\__|_||_\\___\\__|_\\_\\___|_|  \r\n                                                    \r\n\r\n");
-
-const envValues: Record<string, string | undefined> = {};
-const missingEnvKeys: string[] = [];
-
-
-function requireEnvVar(key: string, defaultValue?: string): string {
-  let value = process.env[key] ?? defaultValue;
-  if (value === undefined)
-  {
-    missingEnvKeys.push(key);
-    value = ''
-  }
-  envValues[key] = value;
-  return value;
-}
-
-export const KEYCLOAK_CLIENT_ID_OPENDATA = 'opendata';
-
-export const LOG_LEVEL = requireEnvVar('LOG_LEVEL', 'minimal');
-export const DATABASE_URL = requireEnvVar('DATABASE_URL');
-export const KEYCLOAK_BASE_URL = requireEnvVar('KEYCLOAK_BASE_URL','');
-export const KEYCLOAK_CLIENT_ID = requireEnvVar('KEYCLOAK_CLIENT_ID', KEYCLOAK_CLIENT_ID_OPENDATA);
-export const KEYCLOAK_REALM = requireEnvVar('KEYCLOAK_REALM','');
-export const KEYCLOAK_CLIENT_SECRET = requireEnvVar('KEYCLOAK_CLIENT_SECRET','');
-export const METADATA_BASE_URL = requireEnvVar('METADATA_BASE_URL');
-export const DATASET_CONTENT_PAGE_LIMIT = requireEnvVar('DATASET_CONTENT_PAGE_LIMIT');
-export const DEBUG_MODE_CACHE_ON = requireEnvVar('DEBUG_MODE_CACHE_ON', 'false').trim().toLowerCase() === 'true';
-export const KEYCLOAK_ASSOCIATED_ROLE = requireEnvVar('KEYCLOAK_ASSOCIATED_ROLE', KEYCLOAK_CLIENT_ID_OPENDATA)
-
-
-Object.entries(envValues).forEach(([key, value]) => {
-  const ok = missingEnvKeys.includes(key) ? '❌' : '✅';
-  console.log(`${ok} ${key} = ${value}`);
-});
-
-if (missingEnvKeys.length > 0) {
-  console.error(`Give up, missing required environment variable, look for ❌`);
-  process.exit(1);
-}
 
 interface MetadataDatasetImageGallery {
     ImageUrl: string
